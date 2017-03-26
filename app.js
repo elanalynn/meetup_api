71b2f32372d91e44beb412291280cc415915180b
Elana Kopelevich <ekopelevich@gmail.com>
2017-03-25 20:39:08 -0600

    install/configure cors(*)

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var express = require('express')
-var path = require('path')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-var routes = require('./routes/index')
-var speakers = require('./routes/speakers')
+const routes = require('./routes/index')
+const speakers = require('./routes/speakers')
 
-var app = express()
+const app = express()
 
 app.use(logger('dev'))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
